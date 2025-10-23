66c6dee0145123c7cd1de3e97f9c090ac51729a9
hegdeanusha2415-sudo <hegdeanusha2415@gmail.com>
2025-10-23 10:03:38 +0530

    Updated db.js to use process.env.MONGO_URI

--- config/db.js
+++ config/db.js
@@ -2,11 +2,14 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/clinigoal", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    
+    const conn = await mongoose.connect(
+      process.env.MONGO_URI || "mongodb://localhost:27017/clinigoal",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+
     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`❌ Error: ${error.message}`);
